caa7f1c92be1bece256e6e4cc960ac4aed5a8e13
purplehays09 <purplehays527@gmail.com>
2020-08-13 16:29:02 -0600

    its finally working, most of the style is set up and now i just have to tweak what is left

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from "react";
 import "./App.css";
 import {Header} from './header'
 import {Potd} from './potd'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  background-color:${props => props.theme.color1};
+  color:${props => props.theme.white};
+  width:100%
+`
 
 function App() {
   return (
-    <div className="App">
+    <StyledApp className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        {/* Read through the instructions in the README.md file to build your NASA
+        app! Have fun <span role="img" aria-label='go!'>🚀</span>! */}
       </p>
 
       <Header />
 
       <Potd />
-    </div>
+    </StyledApp>
   );
 }
 
---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+const theme = {
+    color1:'#1F363D',
+    color2:'#40798C',
+    color3:'#70A9A1',
+    color4:'#9EC1A3',
+    color5:'#CFE0C3',
+    paddingSmall:'1%',
+    paddingMedium:'3%',
+    white:'whitesmoke',
+    black: '#2f3236',
+    marginBig:'3%',
+    marginSmall:'1.5%'
+}
+
+export default theme
